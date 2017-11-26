dd726243f57480d82a361021871e0dd88095937c
qiuncheng <qiuncheng@gmail.com>
2017-11-26 15:57:01 +0800

    Update folders, add multi-language support

---
+++ CuteClock/Controller/MainController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  CuteClock
+//
+//  Created by vsccw on 2017/11/26.
+//  Copyright © 2017年 vsccw. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UINavigationController {
+    
+}
+
